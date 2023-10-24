7a1bcba8403caf54bb0d9c5641b36ae7e9862706
yuyafunabashi <yuyafunabashi@pm.me>
2023-10-24 10:09:21 +0000

    feat: cmp symbol for Copilot

--- nvim/.config/nvim/lua/yuyafunabashi/plugins/codeium.lua
+++ nvim/.config/nvim/lua/yuyafunabashi/plugins/codeium.lua
@@ -2,16 +2,18 @@ return {
   "Exafunction/codeium.vim",
   event = "BufEnter",
   config = function()
-    vim.keymap.set("i", "<C-g>", function()
+    local keymap = vim.keymap
+
+    keymap.set("i", "<C-g>", function()
       return vim.fn["codeium#Accept"]()
     end, { expr = true, desc = "Accept completion" })
-    vim.keymap.set("i", "<C-;>", function()
+    keymap.set("i", "<c-;>", function()
       return vim.fn["codeium#CycleCompletions"](1)
     end, { expr = true, desc = "Cycle completions" })
-    vim.keymap.set("i", "<C-,>", function()
+    keymap.set("i", "<c-,>", function()
       return vim.fn["codeium#CycleCompletions"](-1)
     end, { expr = true, desc = "Cycle completions" })
-    vim.keymap.set("i", "<C-x>", function()
+    keymap.set("i", "<c-x>", function()
       return vim.fn["codeium#Clear"]()
     end, { expr = true, desc = "Clear completions" })
   end,
